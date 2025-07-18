64dbbdc6202ee8507f53a5759256575ba004bdde
Shaina G <shainagtherapy@gmail.com>
2025-07-18 10:18:09 -0700

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
 
 // "GET /" per server's first message
 app.get('/', async (req, res) => {
